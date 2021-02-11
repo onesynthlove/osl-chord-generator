81c44be1afea8b794d67d688f7fa6348a569714f
onesynthlove <dev@onesynthlove.com>
2021-02-11 13:15:19 -0800

    test

--- 001_helloworld.py
+++ 001_helloworld.py
@@ -9,11 +9,4 @@ semitones = ["A","Bb","B","C","C#","D","Eb","E","F","Gb","G","Ab","A","Bb","B"]
 
 for semitone in semitones:
 	print("Fuck Yeah. Check out this tone: " + semitone)
-
-
-
-
-
-
-
-
+ 
